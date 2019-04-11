a798ecc2ce009de106e5e1c71e8b70149629f002
flavio@anakin <crsf@viessmann.com>
2019-04-11 11:41:43 +0200

    creato file per aggiornare le modifiche ai file

---
+++ aggiornaFile.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+echo "---- Copio file dalle varie origini ----"
+# mv ~/.gitconfig ./.gitconfig
+# mv ~/.zshrc ./.zshrc
+# mv ~/.config/nvim/init.vim ./init.vim
+
+giorno=`date +%Y-%m-%d`
+ora=`date +%T`
+
+echo "---- Stage modifiche ----"
+
+git add .
+
+echo "---- Creo commit ----"
+
+git commit -m "Modifiche in data e ora $giorno $ora"
+
+echo "---- Push modifiche su GitHub"
+
+git push origin
+
+echo "---- FINITO, puoi tornare a fare danni ----"
